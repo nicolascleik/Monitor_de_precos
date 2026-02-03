95d11785fc761732108eb4873bf37dce7c5f938a
Nicolas Cleik <nicolas@cleik.dev>
2026-02-03 10:21:11 -0300

    Adição do middlewares

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from "express";
 import router from "./src_node/routes/produtoRoutes.js"
 
-const app = express()
+import { logger } from "./src_node/middlewares/Logger.js"
+
+import path from "path"; 
+import { fileURLToPath } from "url";
+
+const urlAtual = import.meta.url
+const __filename = fileURLToPath(urlAtual);
+const __dirname = path.dirname(__filename)
+
+export const app = express()
 const PORT = 3000
 
+app.use(logger);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(router);
 
 app.listen(PORT, () => {
